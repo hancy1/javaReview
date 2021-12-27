4ac52fb86de94ee1108191346a5395055ec48807
codud <codud@192.168.168.110>
2021-12-27 16:32:36 +0900

    채영 변수 형변환 과제

---
+++ src/chap01/chap01_한채영/practice1/run/Run.java
@@ -0,0 +1,15 @@
+package chap01.chap01_한채영.practice1.run;
+
+import chap01.chap01_한채영.practice1.example.VariablePractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+
+		VariablePractice a = new VariablePractice();
+		a.method4();
+
+	}
+
+}
---
+++ src/chap01/chap01_한채영/practice2/run/Run.java
@@ -0,0 +1,14 @@
+package chap01.chap01_한채영.practice2.run;
+
+import chap01.chap01_한채영.practice2.example.CastingPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		CastingPractice a = new CastingPractice();
+		a.method1();
+		
+	}
+
+}
