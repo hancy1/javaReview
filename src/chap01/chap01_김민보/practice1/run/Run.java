84ae1ded8af8e4f62732f40a5f99bf5d976668ba
minBo1119 <monalog17@gmail.com>
2021-12-27 21:02:33 +0900

    변수, 형변환 실습과제 복습 _김민보

---
+++ src/chap01/chap01_김민보/practice1/run/Run.java
@@ -0,0 +1,18 @@
+package chap01.chap01_김민보.practice1.run;
+
+import chap01.chap01_김민보.practice1.example.VariablePractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		VariablePractice a = new VariablePractice();		
+		//a.method1();
+		//a.method2();
+		//a.method3();
+		a.method4();
+		
+	}
+
+}
