6d1147535c7b05c35e619238a0478727b4f6a6b4
codud <codud@192.168.168.110>
2021-12-28 15:47:33 +0900

    한채영 제어문,연산자 복습

---
+++ src/chap02/chap02_한채영/practice/run/Run.java
@@ -0,0 +1,16 @@
+package chap02.chap02_한채영.practice.run;
+
+import chap02.chap02_한채영.practice.example.ConditionPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+//		OperatorPractice a = new OperatorPractice();
+//		a.practice7();
+
+		ConditionPractice b = new ConditionPractice();
+		b.practice10();
+	}
+
+}
