d81ca7223b42a211699885ea141ad67878929152
codud <codud@192.168.168.110>
2022-01-04 21:58:47 +0900

    채영 이차원 배열 복습

---
+++ src/chap05/한채영/run/Run.java
@@ -0,0 +1,14 @@
+package chap05.한채영.run;
+
+import chap05.한채영.dimension.DimensionPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		DimensionPractice a = new DimensionPractice();
+		a.practice4();
+		
+	}
+
+}
