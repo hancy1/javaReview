7b73d4ff9075c78ea6abd6c45d89f2b92390f273
minBo1119 <monalog17@gmail.com>
2022-01-05 23:06:17 +0900

    2차원 배열 김민보

---
+++ src/chap05/김민보/Run.java
@@ -0,0 +1,14 @@
+package chap05.김민보;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		DimensionPractice a = new DimensionPractice();
+		
+		a.practice8();
+	}
+
+}
