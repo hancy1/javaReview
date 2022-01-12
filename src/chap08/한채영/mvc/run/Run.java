046dbb1e0e3b9e5b0f06e07f1383f2d226f7e781
codud <codud@192.168.168.110>
2022-01-12 21:28:59 +0900

    채영 mvc static연습

---
+++ src/chap08/한채영/mvc/run/Run.java
@@ -0,0 +1,17 @@
+package chap08.한채영.mvc.run;
+
+import chap08.한채영.mvc.view.MemberView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	MemberView mv = new MemberView();
+	mv.mainMenu();
+	
+	
+	}
+
+}
