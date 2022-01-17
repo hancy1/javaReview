68b379b1112ac77c2811528b5f42c3a9f7c0665d
minBo1119 <monalog17@gmail.com>
2022-01-18 01:18:58 +0900

    상속 hw1 커밋 - 민보

---
+++ src/chap10/김민보/hw1/model/vo/Student.java
@@ -0,0 +1,43 @@
+package chap10.김민보.hw1.model.vo;
+
+public class Student extends Person {
+
+	private int grade;
+	private String major;
+	
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, int age, double height, double weight, int grade, String major) {		
+				
+		super(age, height, weight);	
+		
+		super.name = name;//네임 직접 접근...
+		
+		this.grade = grade;
+		this.major = major;		
+		
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	
+
+	public String information() {
+		return super.information() + ", 학년 : " + grade + ", 전공 : " + major;
+	}
+	
+	
+	
+}
